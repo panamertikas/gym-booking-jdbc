b9c03b24de03c649866f013411181b4674e7f354
panamertikas <mertikas91@gmail.com>
2026-05-20 18:29:34 +0300

    feat: Test add and delete member CRUD operations

--- src/main/java/com/gymbooking/Main.java
+++ src/main/java/com/gymbooking/Main.java
@@ -18,8 +18,14 @@ public class Main {
         Member m1 = Member.of("Panos", "Mertikas", "panos@gmail.com", 25);
         memberService.addMember(m1);
 
+        Member m2 = Member.of("Maria", "Papadopoulou", "maria@gmail.com", 30);
+        memberService.addMember(m2);
+        memberService.deleteMember(m2);
+
         List<Member> members = memberService.getAllMembers();
         members.forEach(System.out::println);
+
+        memberService.deleteMember(m1);
 
         emf.close();
     }
