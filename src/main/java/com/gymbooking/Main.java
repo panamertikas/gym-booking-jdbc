31e8b76ba74d6b8370578e70a5f60bb8c9bd8082
panamertikas <mertikas91@gmail.com>
2026-05-20 18:24:01 +0300

    feat: Test CRUD operations - add member to database

--- src/main/java/com/gymbooking/Main.java
+++ src/main/java/com/gymbooking/Main.java
@@ -1,5 +1,7 @@
 package com.gymbooking;
 
+import com.gymbooking.model.Member;
+import com.gymbooking.service.MemberService;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
@@ -7,6 +9,13 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("gymBookingPU");
         System.out.println("Connection successful!");
+
+
+        MemberService memberService = new MemberService(emf);
+
+        Member m1 = Member.of("Panos", "Mertikas", "panos@gmail.com", 25);
+        memberService.addMember(m1);
+
         emf.close();
     }
 }
